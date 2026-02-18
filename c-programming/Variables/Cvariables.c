86d7afc6daf9e3798aac84f3c9df502e7cad871c
Ismail Abdalla <ismailmahgoubismailabdalla@gmail.com>
2026-02-18 17:50:13 +0100

    new1

---
+++ c-programming/Variables/Cvariables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int	i; // Holds signed integers, e.g. -4, -2, 0, 1, 10
+	float	f; // Holds sigend floating point numbers, e.g. -3.1416
+	char	c; // Holds character, like a, b, ...
+	
+	i = 2;
+	f = 2.55;
+	c = 'C';
+	printf("%d\n", i);
+	printf("%f\n", f);
+	printf("%c\n", c);
+}
---
+++ c-programming/Variables/integer_in_c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int a = -1;
+	int b = 0;
+	int c = 1;
+	int d = 146858894;
+	int e = -146858894;
+
+	printf("%d\n", a);
+	printf("%d\n", b);
+	printf("%d\n", c);
+	printf("%d\n", d);
+	printf("%d\n", e);
+	return 0;
+}
